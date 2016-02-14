22ee71033312917b0d4bb2c65d5758a40d08e1b7
Vincent Davis <vincent@vincentdavis.net>
2016-02-14 07:54:34 -0700

    no message

--- image_tests.py
+++ image_tests.py
@@ -10,18 +10,18 @@ def pil_verify(ifile):
                 im.verify()
                 return True
             except Exception as e:
-                print(e)
-                print('Pil.Image.verify() failed: ' + ifile)
+                #print(e)
+                #print('Pil.Image.verify() failed: ' + ifile)
                 return False
 #    except OverflowError as e:
 #         print(e)
 #         print('Python int too large to convert to C long: Is this a PSD file: ' + afile)
 #         return False
     except IOError:
-        print('PIL cannot identify image file: ' + ifile)
+        #print('PIL cannot identify image file: ' + ifile)
 #         #testlog.write('> PIL cannot identify image file \n')
 #         #testlog.write('>' + ifile + '\n')
-        return None
+        return False
 #     except Exception as e:
 #         print(e)
 #         print(ifile)
@@ -34,13 +34,12 @@ def exif_test(ifile):
         try:
             with open(ifile, 'rb') as im:
                 tags = exifread.process_file(im, strict=True)
-                tag_count = len(tags)
             try:
                 anydate = any([True for i in tags if 'Date' in i])
             except:
                 anydate = None
-            return True, tag_count, anydate
+            return True, tags, anydate
         except Exception as e:
-            print(e)
-            print('Failed exif: ' + ifile)
+            #print(e)
+            #print('Failed exif: ' + ifile)
             return False, None, None
--- scanning.py
+++ scanning.py
@@ -1,5 +1,7 @@
 from .image_tests import pil_verify, exif_test
+from .data import Images
 import os
+import shutil
 #import .EXIF
 import exifread
 
@@ -15,6 +17,42 @@ def get_file_extensions(path, length):
     print(ftypes)
     return ftypes
 
+##############################################################
+
+imagetracker = dict()
+
+
+def image_sorter(keepfolder = '', discardfolder = '', dryrun = True):
+    """
+    sorts photos into keepfolder or discard folder.
+    Images place in keepfolder id duplicate are placed in keepfolder/dup2
+    :param dryrun:
+    :param discardfolder:
+    :param keepfolder:
+    :return: True (success), False (failed)
+    """
+    total_files = 0
+    flist = []
+    for rpath, dirlist, filelist in os.walk(keepfolder):
+        # scan folder first
+        for i in filelist:
+            flist.append(i)
+    print(len(flist))
+
+    for i in Images.select().where(Images.Exif_Test==True, Images.Pil_Test==True, Images.DestinationFolder == None):
+        #print(i.Exif_Test, i.Pil_Test,i.FullPath, i.DestinationFolder)
+        if i.File_Name not in flist:
+            try:
+                i.DestinationFolder = shutil.move(i.FullPath, keepfolder)
+                flist.append(i.File_Name)
+                if total_files % 250 == 0:
+                    print(total_files)
+                    print(i.Exif_Test, i.Pil_Test,i.FullPath)
+                total_files += 1
+            except Exception as e:
+                i.DestinationFolder = e
+
+
 
 def image_scanner(path, types={'.jpg', '.jpeg', '.tif'}):
     total_files = 0
@@ -24,16 +62,90 @@ def image_scanner(path, types={'.jpg', '.jpeg', '.tif'}):
         Walk returns a lists of file in each directory
         '''
         for afile in filelist:
-            if os.path.splitext(afile)[1].lower() in types and not afile.startswith('.') and '/Masters/' in rpath:
+            if os.path.splitext(afile)[1].lower() in types and not afile.startswith('.') and ('/Masters/' in rpath or '/Originals' in rpath or '/Recovered Photos' in rpath):
                 ifile = os.path.join(rpath, afile) #get full file path
                 total_files += 1 #keep a count of the files
+                #Pil Test
                 pil_result = pil_verify(ifile)
-                if pil_result==False:
-                    print('Fail Pil_verify:\n' + ifile)
+                # if pil_result==False:
+                #     print('Fail Pil_verify:\n' + ifile)
                 #EXIF test
                 exif_result = exif_test(ifile)
-                if exif_result[0] == False:
-                    print('Fail exif_test:\n' + ifile)
+                # if exif_result[0] == False:
+                #     print('Fail exif_test:\n' + ifile)
                 if total_files % 250 == 0:
                     print(total_files)
+                try:
+                    Images.create(FullPath=ifile, Path=rpath, File_Name=afile, Exif_Test=exif_result[0], ExifDate=exif_result[2],
+                              ExifTags=exif_result[1], Pil_Test=pil_result)
+                except:
+                    pass
     print('Done!')
+
+    # FullPath = CharField(unique=True)
+    # Path = CharField()
+    # File_Name = CharField()
+    # Exif_Test = Bool()
+    # ExifDate = CharField()
+    # ExifTags = CharField()
+    # Pil_Test = Bool()
+    # Timestamp = DateTimeField(default=datetime.datetime.now)
+
+
+def delete_faces(path, definition, kbsize=150):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            fname = os.path.splitext(name)[0].lower()
+            if definition in fname:
+                fpath = os.path.join(root, name)
+                fsize = os.path.getsize(fpath)
+                if fsize <= kbsize * 1000:
+                    #print(name, fsize)
+                    try:
+                        shutil.move(fpath, "/Volumes/Drobo/FACES/")
+                    except Exception as e:
+                        if "already exists" in str(e):
+                            os.remove(fpath)
+
+
+def flatten_files(search_path, to_path, move=True):
+    moved = 0
+    discard = 0
+    for root, dirs, files in os.walk(search_path, topdown=False):
+        for name in files:
+            fpath = os.path.join(root, name)
+            if move:
+                try:
+                    shutil.move(fpath, to_path)
+                    moved += 1
+                except Exception as e:
+                    if "already exists" in str(e):
+                        os.remove(fpath)
+                        discard += 1
+                    else:
+                        print('Error {}'.format(fpath))
+            if not move:
+                try:
+                    shutil.copy(fpath, to_path)
+                    moved += 1
+                except Exception as e:
+                    if "already exists" in str(e):
+                        discard += 1
+                    else:
+                        print('Error {}'.format(fpath))
+    print('Moved: {}. Discarded: {}'.format(moved, discard))
+
+
+def delete_small(path, kbsize, match):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            fname = os.path.splitext(name)[1].lower()
+            fpath = os.path.join(root, name)
+            fsize = os.path.getsize(fpath)
+
+            if fname in match and fsize <= kbsize * 1000:
+                #print(fname, fsize)
+                os.remove(fpath)
+
+
+
